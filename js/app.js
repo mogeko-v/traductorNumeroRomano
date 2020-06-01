e631caa82e0564c9d70a47d42051a34f0e3ec2f6
mogeko-v <64927836+mogeko-v@users.noreply.github.com>
2020-06-01 10:52:12 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+
+
+
+function calcularRomano() {
+    var numeroEntrada = parseInt(document.getElementById("inputNumber").value) 
+
+
+    console.log(numeroEntrada);
+
+    
+    var resultado = document.getElementById("pResultado")
+    resultado.innerHTML = romanize(numeroEntrada)
+    
+
+}
+
+
+
+function romanize (num) {
+    if (!+num)
+        return false;
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
+
+romanize(numeroEntrada)
